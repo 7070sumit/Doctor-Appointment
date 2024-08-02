ab3021368517dfb8020d5dc8d5dc59a1f20fe435
Roshan Prasad Sahu <roshanprasad447@gmail.com>
2024-08-02 18:48:37 +0530

    responsiveHeader

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ import {Link,NavLink} from 'react-router-dom'
 function Header() {
   return (
     <header className='shadow sticky z-50 top-0' >
-      <nav className='bg-cyan-400 border-gray-200 px-4'>
+      <nav className='  px-4 border-1/2 border-[#490B3D]'>
         <div className='flex flex-wrap justify-center text-center items-center gap-1 mx-2'>
             <div className='basis-1/4'>
                 <Link to="/" className='flex mr-3 h-12'>
@@ -13,33 +13,40 @@ function Header() {
                 </Link>
             </div>
             <div className='basis-1/2 justify-between items-center w-full'>
-                <ul className='flex flex-row font-medium  justify-around'>
-                  <li >
-                    <NavLink to="/" className={({isActive})=> `${isActive ? "text-[#490B3D]":"text-white"} hover:bg-cyan-200 hover:font-bold   `} >
+                <ul className='sm:flex sm:gap-2 flex-row font-medium  justify-around  hidden'>
+                  <li className='hover:underline hover:decoration-[#BD1E51]' >
+                    <NavLink to="/" className={({ isActive }) =>
+                        `${isActive ? "text-[#F1B814]" : ""} hover:text-[#F1B814] duration-100`
+                    } >
                       Home
                     </NavLink>
                   </li>
-                  <li>
-                    <NavLink to="/" className={({isActive})=> `${isActive ? "text-[#490B3D]":"text-white"}  hover:bg-cyan-200  hover:font-bold `} >
+                  <li className='hover:underline hover:decoration-[#BD1E51]'>
+                    <NavLink to="/labtest" className={({ isActive }) =>
+                        `${isActive ? "text-[#F1B814]" : ""} hover:text-[#F1B814] duration-100`
+                    } >
                       Lab Tests
                     </NavLink>
                   </li>
-                  <li>
-                    <NavLink to="/" className={({isActive})=> `${isActive ? "text-[#490B3D]":"text-white"} hover:bg-cyan-200 hover:font-bold `} >
+                  <li className='hover:underline hover:decoration-[#BD1E51]'>
+                    <NavLink to="/buymedicine" className={({ isActive }) =>
+                        `${isActive ? "text-[#F1B814]" : ""} hover:text-[#F1B814] duration-100`
+                    }>
                       Buy Medicine
                     </NavLink>
                   </li>
-                  <li>
-                    <NavLink to="/" className={({isActive})=> `${isActive ? "text-[#490B3D]":"text-white"} hover:bg-cyan-200  hover:font-bold `} >
+                  <li className='hover:underline hover:decoration-[#BD1E51]'>
+                    <NavLink to="/videoconsult" className={({ isActive }) =>
+                        `${isActive ? "text-[#F1B814]" : ""} hover:text-[#F1B814] duration-100`
+                    } >
                       Video Consult
                     </NavLink>
                   </li>
                 </ul>
             </div>
             <div className='basis-1/5  '>
-            <Link to="/LogIn" className='  hover:bg-cyan-200  hover:font-bold'>Log In </Link>
-            /
-            <Link to="/SignUp" className='  hover:bg-cyan-200  hover:font-bold'> Sign Up</Link>
+            <Link to="/user/login" className=' border-2 border-[#F1B814] bg-[#BD1E51]  rounded-md text-white font-semibold hover:bg-[#86176f]  p-1'>Log In / Sign Up </Link>
+            
             </div>
         </div>
       </nav>
