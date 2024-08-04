e0807f9c27b245fe26c8da27f9191e5b4780da5a
Roshan Prasad Sahu <roshanprasad447@gmail.com>
2024-08-04 12:55:37 +0530

    footerAdded

--- Frontend/src/Components/Footer/Footer.jsx
+++ Frontend/src/Components/Footer/Footer.jsx
@@ -3,7 +3,126 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-      <h1>This is Footer</h1>
+      <footer>
+        <div className='flex   justify-evenly bg-[#F1B814]'>
+          <div className='p-4'>
+            <h1 className=' font-serif font-extrabold text-2xl'>Still delaying your health concerns ?</h1>
+            <h6 className=' font-serif '>Connect with India's top doctors online</h6>
+          </div>
+          <div className='p-4'>
+            <button className='bg-[#BD1E51] text-white text-2xl font-serif font-extrabold border-2 border-[#490B3D] rounded-xl shadow-lg shadow-[#490B3D] px-20 py-4'>Consult Now</button>
+          </div>
+        </div>
+
+
+
+        <div className='flex justify-evenly bg-slate-200'>
+          <div className='mx-20 my-5'>
+            <ul className='text-slate-700'>
+              <h1 className='text-black font-sans font-semibold mb-2'>About XYZ Hospital</h1>
+              <li>About Us</li>
+              <li>Contact Us</li>
+              <li>FAQs</li>
+              <li>Health Queries</li>
+              <li>Terms & Condition</li>
+              <li>Return Policy</li>
+              <li>Refund Policy</li>
+              <li>Privacy Policy</li>
+              <li>Corporate Disclosures</li>
+              <li>XYZ hospital pharmacy</li>
+            </ul>
+          </div>
+          <div className='mx-20 my-5'>
+            <ul className='text-slate-700'>
+              <h1 className='text-black font-sans font-semibold mb-2'>Services</h1>
+              <li>Online Consultation</li>
+              <li>XYZ Membership</li>
+              <li>Online Medicines</li>
+              <li>XYZ Pro Health Program</li>
+              <li>Doctors by Speciality</li>
+              <li>Doctors by City</li>
+              <li>All Doctors List</li>
+            </ul>
+          </div>
+          <div className='mx-20 my-5'>
+            <ul className='text-slate-700'>
+              <h1 className='text-black font-sans font-semibold mb-2'>Top Speciality</h1>
+              <li>Consult Physicians</li>
+              <li>Consult Dermatologists</li>
+              <li>Consult Paediatricians</li>
+              <li>Consult Gyanecologists</li>
+              <li>Consult Cardiologists</li>
+              <li>Consult Dietitians</li>
+              <li>Consult ENT Specialists</li>
+              <li>Consult Geriatricians</li>
+              <li>Consult Diabetologists</li>
+            </ul>
+          </div>
+          <div className='mx-20 my-5'>
+            <ul className='text-slate-700'>
+              <h1 className='text-black font-sans font-semibold mb-2'>Book Lab Test At Home</h1>
+              <li>RT PCR Test At Home</li>
+              <li>Book Lab Test At Home</li>
+              <li>Renal Profile (KFT, RFT Test)</li>
+              <li>Hemogram Test</li>
+              <li>Urine Culture Test</li>
+              <li>Widal Test</li>
+              <li>LFT Test</li>
+              <li>Lipid Test</li>
+              <li>D Dimer Test</li>
+              <li>CBC Test</li>
+              <li>T3 T4 Tsh Test</li>
+            </ul>
+          </div>
+          <div className='mx-20 my-5'>
+            <ul className='text-slate-700'>
+              <h1 className='text-black font-sans font-semibold mb-2'>Product Categories</h1>
+              <li>View All</li>
+              <li> Health Care</li>
+              <li>Nutrition</li>
+              <li>Healthcare Devices</li>
+              <li>Immunity Boosters</li>
+              <li>Diabetes Care</li>
+              <li>Corona Virus Prevention</li>
+            </ul>
+          </div>
+        </div>
+
+
+
+        <div className='flex justify-evenly p-5 bg-slate-200 border-t-2 border-slate-300'>
+          <div>Logo</div>
+
+
+          <div>
+            <div className='text-center'>Get XYZ Hospital Mobile App</div>
+            <div className='flex justify-evenly '>
+              <div className='flex bg-slate-500 rounded-lg m-1 px-8 p-2'>
+              <svg width="43px" height="43px" viewBox="-1.5 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>google_play [#133]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-101.000000, -7599.000000)" fill="#000000"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M45,7458.928 L45,7459 L53.5,7454 L56.5956547,7452.185 L54.1323291,7449.705 L45,7458.928 Z M57.4515643,7446.334 L54.8020857,7449 L57.4515643,7451.667 L62,7449 L57.4515643,7446.334 Z M56.5670915,7445.816 L53.5,7444 L45,7439 L45,7439.072 L54.1323291,7448.296 L56.5670915,7445.816 Z M53.469467,7448.967 L45,7457.517 L45,7449 L45,7440.423 L53.469467,7448.967 Z" id="google_play-[#133]"> </path> </g> </g> </g> </g></svg>
+                <h6 >GET IN ON <br /> Google Play</h6>
+                
+              </div>
+              <div className='flex bg-slate-500 rounded-lg m-1 px-8 p-2'>
+              <svg fill="#000000" width="43px" height="43px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18.71 19.5C17.88 20.74 17 21.95 15.66 21.97C14.32 22 13.89 21.18 12.37 21.18C10.84 21.18 10.37 21.95 9.09997 22C7.78997 22.05 6.79997 20.68 5.95997 19.47C4.24997 17 2.93997 12.45 4.69997 9.39C5.56997 7.87 7.12997 6.91 8.81997 6.88C10.1 6.86 11.32 7.75 12.11 7.75C12.89 7.75 14.37 6.68 15.92 6.84C16.57 6.87 18.39 7.1 19.56 8.82C19.47 8.88 17.39 10.1 17.41 12.63C17.44 15.65 20.06 16.66 20.09 16.67C20.06 16.74 19.67 18.11 18.71 19.5ZM13 3.5C13.73 2.67 14.94 2.04 15.94 2C16.07 3.17 15.6 4.35 14.9 5.19C14.21 6.04 13.07 6.7 11.95 6.61C11.8 5.46 12.36 4.26 13 3.5Z"></path> </g></svg>
+              <h6>GET IT ON <br /> App Store</h6>
+              </div>
+            </div>
+          </div>
+
+
+          <div className=''>
+            <div className='text-center'>Find Us</div>
+            <div className=''>
+              <ul className='flex gap-5'>
+                <li>FaceBook</li>
+                <li>LinkedIn</li>
+                <li>Twitter</li>
+                <li>Youtube</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   )
 }
