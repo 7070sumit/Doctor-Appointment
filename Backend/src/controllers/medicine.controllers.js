519e95c921f775f670f746eee5da2dc6d005dd77
Sumit Kumar Mahto <sumit.kumar.mahto341@gmail.com>
2024-08-11 10:15:00 +0530

    req limit added

--- Backend/src/controllers/medicine.controllers.js
+++ Backend/src/controllers/medicine.controllers.js
@@ -4,23 +4,29 @@ import { ApiResponse } from '../utils/ApiResponse.js'
 import {Medicine} from '../models/medicine.model.js'
 
 
-const getAllMedicine=asyncHandler(async(req,res)=>{
-    
-    const medicine=await Medicine.find({}).limit(5)
-    if (medicine){
+const getAllMedicine = asyncHandler(async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 15; 
+
+    const skip = (page - 1) * limit;
+
+    const medicine = await Medicine.find({}).skip(skip).limit(limit);
+
+    if (medicine) {
         return res
-        .status(200)
-        .json(
-            new ApiResponse(
-                200,
-                medicine,
-                "Medicine fetched successfully"
-            )
-        )
-    }else{
-        throw new ApiError(500,{},"Could not fetch medicine information.")
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    medicine,
+                    "Medicine fetched successfully"
+                )
+            );
+    } else {
+        throw new ApiError(500, {}, "Could not fetch medicine information.");
     }
-})
+});
+
 
 
 
